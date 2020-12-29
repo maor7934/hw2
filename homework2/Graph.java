fe1e476f4fa910685bcdff13348b8582567c03d2
Maor <maor7934@gmail.com>
2020-12-29 16:50:04 +0200

    tests

--- homework2/Graph.java
+++ homework2/Graph.java
@@ -1,101 +1,126 @@
 package homework2;
+
 import java.util.*;
 
+class AlreadyHasNodeException extends Exception {
+
+}
+
+class AlreadyHasEdgeException extends Exception {
+
+}
+
+class NodeNotInGraphExpection extends Exception {
+
+}
+
 public class Graph<T> {
-	
+
 	/**
-	 * <b>Abstract Function-</b>
-	 * 	The class contains all the nodes of certain graph, with it's connectivity (edges) between nodes.
-	 * <b>Representation Invariant-</b>
-	 * 		all the edges must be contained inside the graph.
-	 * 		no null nodes.
+	 * <b>Abstract Function-</b> The class contains all the nodes of certain graph,
+	 * with it's connectivity (edges) between nodes. <b>Representation
+	 * Invariant-</b> all the edges must be contained inside the graph. no null
+	 * nodes.
 	 */
 	private void checkRep() {
-		assert
-			this.nodeCollection != null :
-				"Rep. Inv. of class is violeted";
-	}
-	
-	private HashMap<T, HashSet<T>> nodeCollection;
-	
-	//TODO: maybe for checkrep? 
-	private Boolean checkAllSetInGraph(HashSet<T> childrenSet) {
-		
-	     Iterator<T> iChild = childrenSet.iterator();
-	     while(iChild.hasNext()){
-	        T currChild = iChild.next();
-	        if (!this.nodeCollection.containsKey(currChild)){
-	        	return false;
-	        }     
-	     }
-	     return true;
-		
+		assert this.nodeCollection != null : "Rep. Inv. of class is violeted";
 	}
 
-	
-  	/**
-  	 * Constructs new graph object.
-     * @requires none.
-   	 * @effects constructs an empty graph.
-   	 * @modifies this object
-   	 **/  	
+	protected HashMap<T, HashSet<T>> nodeCollection;
+
+	// TODO: maybe for checkrep?
+	private Boolean checkAllSetInGraph(HashSet<T> childrenSet) {
+
+		Iterator<T> iChild = childrenSet.iterator();
+		while (iChild.hasNext()) {
+			T currChild = iChild.next();
+			if (!this.nodeCollection.containsKey(currChild)) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+	/**
+	 * Constructs new graph object.
+	 * 
+	 * @requires none.
+	 * @effects constructs an empty graph.
+	 * @modifies this object
+	 **/
 	public Graph() {
 		this.nodeCollection = new HashMap<T, HashSet<T>>();
 		checkRep();
 	}
-	
-  	/**
-  	 * Adds a new Node to the graph.
-     * @requires the valid String node Name, and a node with type <T>.
-   	 * @effects Adds the node into the graph (adds new object).
-   	 * @modifies this object.
-   	 **/
-	public void addNode(T newnode) {
+
+	/**
+	 * Adds a new Node to the graph.
+	 * 
+	 * @requires the valid String node Name, and a node with type <T>.
+	 * @effects Adds the node into the graph (adds new object).
+	 * @modifies this object.
+	 * @throws expection if Node already exists
+	 **/
+	public void addNode(T new_node) throws AlreadyHasNodeException {
 		checkRep();
-		this.nodeCollection.putIfAbsent(newnode, new HashSet<T>());
-		checkRep();
-	}
-	
-  	/**
-  	 * Adds a new edge between parent and child nodes.
-     * @requires none
-   	 * @effects Adds the edge into the graph (adds new object).
-   	 * @modifies this object.
-   	 * @return true if added the child, false is it didn't (parent or child arn't found in the graph)
-   	 **/
-	public Boolean addEdge(T parentNode, T childNodeName) {
-		checkRep();
-		if (this.nodeCollection.containsKey(parentNode) && this.nodeCollection.containsKey(childNodeName)){
-			return this.nodeCollection.get(parentNode).add(childNodeName);
+		if (this.nodeCollection.containsKey(new_node)) {
+			throw new AlreadyHasNodeException();
+
 		}
-		return false;
-	}
-	
-  	/**
-  	 * the function returns a set of the keys in the graph.
-     * @requires this object is initialized
-   	 * @effects none
-   	 * @modifies none
-   	 * @return set of the nodes in the graph.
-   	 **/
-	public ArrayList<T> getNodes(){
+		this.nodeCollection.putIfAbsent(new_node, new HashSet<T>());
 		checkRep();
-		return new ArrayList<T>(this.nodeCollection.keySet()); //TODO: maybe return hard copy of nodes?
 	}
-	
-  	/**
-  	 * the function returns an arraylist of the children of a given parent nod in the graph.
-     * @requires this object is initialized
-   	 * @effects none
-   	 * @modifies none
-   	 * @return if the parent exist in the graph, we'll return an array list containing its children, and null otherwise.
-   	 **/ 
-	public ArrayList<T> getChildren(T parentNode){
+
+	/**
+	 * Adds a new edge between parent and child nodes.
+	 * 
+	 * @requires none
+	 * @effects Adds the edge into the graph (adds new object).
+	 * @modifies this object.
+	 * @return true if added the child,
+	 * @throws expection is it didn't (parent or child arn't found in the graph)
+	 **/
+	public Boolean addEdge(T parent_node, T child_node_name) throws AlreadyHasEdgeException, NodeNotInGraphExpection {
+		checkRep();
+		if (!(this.nodeCollection.containsKey(parent_node) && this.nodeCollection.containsKey(child_node_name))) {
+			throw new NodeNotInGraphExpection();
+		}
+		if (this.nodeCollection.get(parent_node).contains(child_node_name)) {
+			throw new AlreadyHasEdgeException();
+		}
+		return this.nodeCollection.get(parent_node).add(child_node_name);
+	}
+
+	/**
+	 * the function returns a set of the keys in the graph.
+	 * 
+	 * @requires this object is initialized
+	 * @effects none
+	 * @modifies none
+	 * @return set of the nodes in the graph.
+	 **/
+	public ArrayList<T> getNodes() {
+		checkRep();
+		return new ArrayList<T>(this.nodeCollection.keySet()); // TODO: maybe return hard copy of nodes?
+	}
+
+	/**
+	 * the function returns an arraylist of the children of a given parent nod in
+	 * the graph.
+	 * 
+	 * @requires this object is initialized
+	 * @effects none
+	 * @modifies none
+	 * @return if the parent exist in the graph, we'll return an array list
+	 *         containing its children, and null otherwise.
+	 **/
+	public ArrayList<T> getChildren(T parentNode) {
 		checkRep();
 		if (this.nodeCollection.containsKey(parentNode)) {
-			//return this.nodeCollection.get(parentNode);
-			return new ArrayList<T>(this.nodeCollection.get(parentNode)); //TODO: maybe return hard copy of nodes?
+			// return this.nodeCollection.get(parentNode);
+			return new ArrayList<T>(this.nodeCollection.get(parentNode)); // TODO: maybe return hard copy of nodes?
 		}
-		return null; 
+		return null;
 	}
 }
--- homework2/GraphTests.java
+++ homework2/GraphTests.java
@@ -1,5 +1,11 @@
 package homework2;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 //just comment for testing DAEL
 /**
  * This class contains a set of test cases that can be used to test the graph
@@ -13,6 +19,152 @@ public class GraphTests extends ScriptFileTests {
 		super(testFile);
 	}
 
-	// TODO: add additional white box tests
+	@Test
+	public void check_create_graph() {
+		Graph<Integer> graph = new Graph<>();
+		assertEquals(new HashMap<Integer, HashSet<Integer>>(), graph.nodeCollection);
+	}
+
+	@Test
+	public void check_add_node() {
+		Graph<Integer> graph = new Graph<>();
+		try {
+			graph.addNode(1);
+		} catch (AlreadyHasNodeException e) {
+			e.printStackTrace();
+		}
+		HashMap<Integer, HashSet<Integer>> check_hash = new HashMap<>();
+		check_hash.put(1, new HashSet<Integer>());
+		assertEquals(check_hash, graph.nodeCollection);
+	}
+
+	@Test(expected = AlreadyHasNodeException.class)
+	public void check_add_same_node() throws AlreadyHasNodeException {
+		Graph<Integer> graph = new Graph<>();
+		graph.addNode(123);
+		graph.addNode(123);
+	}
+
+	@Test(expected = NodeNotInGraphExpection.class)
+	public void check_missing_node() throws NodeNotInGraphExpection {
+		Graph<Integer> graph = new Graph<>();
+		graph.getChildren(1);
+	}
+
+	public void check_add_edge() {
+		Graph<Integer> graph = new Graph<>();
+		try {
+			graph.addNode(1);
+			graph.addNode(2);
+			graph.addEdge(1, 2);
+		} catch (AlreadyHasNodeException | NodeNotInGraphExpection | AlreadyHasEdgeException e) {
+			e.printStackTrace();
+		}
+		HashMap<Integer, HashSet<Integer>> check_hash = new HashMap<>();
+		check_hash.put(1, new HashSet<Integer>());
+		check_hash.put(2, new HashSet<Integer>());
+		check_hash.get(1).add(2);
+		assertEquals(check_hash, graph.nodeCollection);
+	}
+
+	@Test(expected = AlreadyHasNodeException.class)
+	public void check_add_same_edge() throws AlreadyHasNodeException, NodeNotInGraphExpection, AlreadyHasEdgeException {
+		Graph<Integer> graph = new Graph<>();
+		graph.addNode(1);
+		graph.addNode(2);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 2);
+	}
+
+	@Test(expected = NodeNotInGraphExpection.class)
+	public void check_add_edge_missing_first_node()
+			throws AlreadyHasNodeException, NodeNotInGraphExpection, AlreadyHasEdgeException {
+		Graph<Integer> graph = new Graph<>();
+		graph.addNode(1);
+		graph.addEdge(1, 2);
+	}
+
+	@Test(expected = NodeNotInGraphExpection.class)
+	public void check_add_edge_missing_second_node()
+			throws AlreadyHasNodeException, NodeNotInGraphExpection, AlreadyHasEdgeException {
+		Graph<Integer> graph = new Graph<>();
+		graph.addNode(2);
+		graph.addEdge(1, 2);
+	}
+
+	@Test(expected = NodeNotInGraphExpection.class)
+	public void check_add_edge_missing_both_node()
+			throws AlreadyHasNodeException, NodeNotInGraphExpection, AlreadyHasEdgeException {
+		Graph<Integer> graph = new Graph<>();
+		graph.addEdge(1, 2);
+	}
+
+	@Test
+	public void check_graph_good_test() {
+		Graph<Integer> graph = new Graph<>();
+		try {
+			graph.addNode(1);
+			graph.addNode(2);
+			graph.addNode(3);
+			graph.addNode(4);
+			graph.addNode(5);
+			graph.addNode(6);
+			graph.addNode(7);
+			graph.addNode(8);
+			graph.addNode(9);
+			graph.addNode(10);
+			graph.addEdge(1, 2);
+			graph.addEdge(1, 3);
+			graph.addEdge(1, 4);
+			graph.addEdge(1, 5);
+			graph.addEdge(1, 6);
+			graph.addEdge(1, 7);
+			graph.addEdge(1, 8);
+			graph.addEdge(1, 9);
+			graph.addEdge(1, 10);
+			graph.addEdge(2, 1);
+			graph.addEdge(3, 2);
+			graph.addEdge(4, 7);
+			graph.addEdge(7, 3);
+			graph.addEdge(3, 4);
+			graph.addEdge(4, 5);
+			graph.addEdge(8, 4);
+			graph.addEdge(8, 1);
+			graph.addEdge(7, 2);
+			graph.addEdge(7, 1);
+
+		} catch (AlreadyHasNodeException | NodeNotInGraphExpection | AlreadyHasEdgeException e) {
+			e.printStackTrace();
+		}
+			assertTrue (graph.nodeCollection.containsKey(1));
+			assertTrue (graph.nodeCollection.containsKey(2));
+			assertTrue (graph.nodeCollection.containsKey(3));
+			assertTrue (graph.nodeCollection.containsKey(4));
+			assertTrue (graph.nodeCollection.containsKey(5));
+			assertTrue (graph.nodeCollection.containsKey(6));
+			assertTrue (graph.nodeCollection.containsKey(7));
+			assertTrue (graph.nodeCollection.containsKey(8));
+			assertTrue (graph.nodeCollection.containsKey(9));
+			assertTrue (graph.nodeCollection.containsKey(10));
+			assertTrue (graph.getChildren(1).contains(2));
+			assertTrue (graph.getChildren(1).contains(3));
+			assertTrue (graph.getChildren(1).contains(4));
+			assertTrue (graph.getChildren(1).contains(5));
+			assertTrue (graph.getChildren(1).contains(6));
+			assertTrue (graph.getChildren(1).contains(7));
+			assertTrue (graph.getChildren(1).contains(8));
+			assertTrue (graph.getChildren(1).contains(9));
+			assertTrue (graph.getChildren(1).contains(10));
+			assertTrue (graph.getChildren(2).contains(1));
+			assertTrue (graph.getChildren(3).contains(2));
+			assertTrue (graph.getChildren(4).contains(7));
+			assertTrue (graph.getChildren(7).contains(3));
+			assertTrue (graph.getChildren(3).contains(4));
+			assertTrue (graph.getChildren(4).contains(5));
+			assertTrue (graph.getChildren(8).contains(4));
+			assertTrue (graph.getChildren(8).contains(1));
+			assertTrue (graph.getChildren(7).contains(2));
+			assertTrue (graph.getChildren(7).contains(1));
+	}
 
 }
